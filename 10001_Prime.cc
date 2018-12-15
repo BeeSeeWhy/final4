994a209a92184d457cae158660c06e236fe5753e
bcruz13 <bcruz13@hills.ccsf.edu>
2018-12-15 10:55:44 -0800

    first commit

---
+++ 10001_Prime.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int check, n = 10001, c = 0;
+
+    for(int i = 2 ;i <= 1000000; i++) {
+        check = 0;
+
+        for(int j=2;j<=i/2;j++) {
+            if(i%j==0) {
+                check=1;
+                break;
+            }
+        }
+
+        if(check == 0)
+            c++;
+        if(c == n) {
+            cout << n << "st Prime Number is: " << i;
+            cout << endl;
+            break;
+        }
+    }
+    return 0;
+}
